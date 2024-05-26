e07ce41e10a30c19421ece0ebe633c333bbebb89
Divo123 <topherharsh@gmail.com>
2024-05-26 09:14:44 +0800

    Fixed state bug and button center alignment

--- client/src/Tipping.jsx
+++ client/src/Tipping.jsx
@@ -2,24 +2,20 @@ import './Tipping.css'
 import { useState } from 'react'
 // import TipButton from './TipButton'
 function Tipping() {
-    const [amount, setAmount] = useState(0)
+    const [amount, setAmount] = useState(null)
     const handleClick = (tip) =>{
-        return () => {
-             setAmount(tip)
+             setAmount(tip);
              console.log(amount)
-        }
-            
-           
-    }
+    };
     return (
         <div className="tippingSection">
             <h1 className="title">APPRECIATE YOUR DRIVER</h1>
             <p className="description">Show your appreciation with a tip!</p>
             <section className="buttonSection">
-                <button className='tipButton' onClick={handleClick(0.1)}>10¢</button>
-                <button className='tipButton' onClick={handleClick(0.5)}>50¢</button>
-                <button className='tipButton' onClick={handleClick(0.2)}>20¢</button>
-                <button className='tipButton' onClick={handleClick(1)}>$1</button>
+                <button className='tipButton' onClick={() => handleClick(0.1)} style={{ color: amount === 0.1 ? 'green' : 'orange', border: `2px solid ${amount === 0.1 ? 'green' : 'orange'}`}}>10¢</button>
+                <button className='tipButton' onClick={() => handleClick(0.5)} style={{ color: amount === 0.5 ? 'green' : 'orange', border: `2px solid ${amount === 0.5 ? 'green' : 'orange'}`}}>50¢</button>
+                <button className='tipButton' onClick={() => handleClick(0.2)} style={{ color: amount === 0.2 ? 'green' : 'orange', border: `2px solid ${amount === 0.2 ? 'green' : 'orange'}`}}>20¢</button>
+                <button className='tipButton' onClick={() => handleClick(1)} style={{ color: amount === 1 ? 'green' : 'orange', border: `2px solid ${amount === 1 ? 'green' : 'orange'}`}}>$1</button>
             </section>
         </div>
     )
